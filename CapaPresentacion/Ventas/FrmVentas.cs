b3839f1d3aaf4c7943f2a5ff7f6deab0bf343099
Derian <derianch37@gmail.com>
2017-11-09 15:33:38 -0600

    Revert "Revert "Ultima Modificacion""
    This reverts commit 434c68f18b98b638fb9b7e397e225b27185de44d, reversing
    changes made to a8ac573fd489cf131c97843b8a321c31cde9037e.

--- CapaPresentacion/Ventas/FrmVentas.cs
+++ CapaPresentacion/Ventas/FrmVentas.cs
@@ -98,9 +98,8 @@ namespace CapaPresentacion
             }
             catch (Exception ex)
             {
-                MessageBox.Show("Digite un código de producto valido", caption: "Alerta", buttons: MessageBoxButtons.OK, icon: MessageBoxIcon.Error);
+                MessageBox.Show("Digite un código de producto valido"+ex, caption: "Alerta", buttons: MessageBoxButtons.OK, icon: MessageBoxIcon.Error);
             }
-            MessageBox.Show("Digite un código de producto valido"+nombreImpresora, caption: "Alerta", buttons: MessageBoxButtons.OK, icon: MessageBoxIcon.Error);
         }
 
         private void BuscarProducto()
